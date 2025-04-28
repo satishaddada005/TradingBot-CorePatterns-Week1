af38dbc9d39df6e57b63bc4ba6e5151c9ec3a516
Satish Addada <sa185403@ncr.com>
2025-04-28 19:54:03 +0530

    Added Classes

---
+++ CorePatterns/CorePatterns.ConsoleApp/Resources/DBConnections/DBConnections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePatterns.ConsoleApp.Resources.DBConnections
+{
+    internal class DBConnections : IDisposable
+    {
+        public void Dispose() { }
+    }
+}
